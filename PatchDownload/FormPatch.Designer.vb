9b881e64979fc282c78165a17995effd0dde78aa
Thryn <52488475+ThrynSec@users.noreply.github.com>
2019-07-10 14:35:38 +0200

    Add files via upload

---
+++ PatchDownload/FormPatch.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormCorrect
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormCorrect))
+        Me.PBar = New System.Windows.Forms.ProgressBar()
+        Me.PatchButton = New System.Windows.Forms.PictureBox()
+        CType(Me.PatchButton, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PBar
+        '
+        Me.PBar.Location = New System.Drawing.Point(50, 88)
+        Me.PBar.Name = "PBar"
+        Me.PBar.Size = New System.Drawing.Size(248, 24)
+        Me.PBar.TabIndex = 0
+        '
+        'PatchButton
+        '
+        Me.PatchButton.BackColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.PatchButton.Image = CType(resources.GetObject("PatchButton.Image"), System.Drawing.Image)
+        Me.PatchButton.Location = New System.Drawing.Point(33, 88)
+        Me.PatchButton.Name = "PatchButton"
+        Me.PatchButton.Size = New System.Drawing.Size(277, 38)
+        Me.PatchButton.TabIndex = 1
+        Me.PatchButton.TabStop = False
+        '
+        'FormCorrect
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(327, 177)
+        Me.Controls.Add(Me.PatchButton)
+        Me.Controls.Add(Me.PBar)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FormCorrect"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Bnet fail correction"
+        CType(Me.PatchButton, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PBar As ProgressBar
+    Friend WithEvents PatchButton As PictureBox
+End Class
---
+++ PatchDownload/FormPatch.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Security.Principal
+
+Public Class FormCorrect
+
+    Dim identity = WindowsIdentity.GetCurrent()
+    Dim program = New WindowsPrincipal(identity)
+    Dim isAdmin As Boolean = program.IsInRole(WindowsBuiltInRole.Administrator)
+    Dim userName As String = Environment.UserName.ToString
+
+    Private Sub FormCorrect_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'ProgramData, le cache principal et celui qui est majoritairement la cause des bugs, est protégé et ne peut être supprimé que par un admin
+
+        If isAdmin = False Then
+            MsgBox("Merci de relancer le programme en mode administrateur", MsgBoxStyle.Critical)
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub ButtonClick(sender As Object, e As EventArgs) Handles PatchButton.Click
+
+        On Error Resume Next
+        System.IO.Directory.Delete("C:\Users\" & userName & "\AppData\Roaming\Battle.net", True)
+        PBar.Value = 20
+
+        System.IO.Directory.Delete("C:\Users\" & userName & "\AppData\Local\Battle.net", True)
+        PBar.Value = 40
+
+        System.IO.Directory.Delete("C:\Users\" & userName & "\AppData\Local\Temp\Battle.net", True)
+        PBar.Value = 65
+
+        System.IO.Directory.Delete("C:\ProgramData\Battle.net", True)
+        PBar.Value = 100
+        On Error GoTo 0
+
+        MsgBox("Suppression des caches terminée ! Relancez votre ordinateur et vous devriez pouvoir installer Battle.net correctement")
+
+        Me.Close()
+
+    End Sub
+End Class
